6a48ce5333332df1189b028525f767ce2c347947
lia-07 <107384739+lia-07@users.noreply.github.com>
2025-06-19 11:58:40 +1200

    add a bunch of comments

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 daily_notes = os.getenv('DAILY_NOTES_DIR')
 
+# a class for creating list item objects, which each have a 'content' and 'ticked' attribute
 class ListItem:
     def __init__(self, md_item):
         self.__content = md_item[6:].rstrip()
@@ -19,11 +20,13 @@ class ListItem:
     def __remove_timestamp(self):
         self.__content = self.__content.split("<")[0].rstrip()
 
+    # setter for ticked attribute
     def tick(self, timestamp):
         if timestamp and not (len(self.__content) > 20 and self.__content[-20] == "<" and self.__content[-1] == ">"):
             self.__add_timestamp()
         self.__ticked = True
 
+    # setter for ticked attribute
     def untick(self):
         if len(self.__content) > 20 and self.__content[-20] == "<" and self.__content[-1] == ">":
             self.__remove_timestamp()
@@ -38,9 +41,11 @@ class ListItem:
     def get_content(self):
         return self.__content
 
+    # returns the object as a markdown string
     def to_md(self):
         return f"- [{'x' if self.__ticked else ' '}] {self.__content}\n"
 
+    # returns the object as an html string
     def to_html(self, i, today):
         return render_template("ListItem.html", ticked=self.__ticked, text=self.__content, i=i, today=today)
 
@@ -58,29 +63,37 @@ def TaskList():
 
     with open(f"{daily_notes}/{today}.md") as note:
         items = ''
+        # loop over each line in the file
         for i,line in enumerate(note):
-            # CHECK IF FILE IS EMPTY
+
+            # check if line is a markdown checklist item, if so create a list item object from it
             if line.startswith("- ["):
-                # line is a list item
                 item = ListItem(line)
 
                 # ensure unticked items don't have timestamps
                 if not item.is_ticked():
                     item.untick()
 
+                # append html version of the list item to the "items" string
                 items += item.to_html(i, today)
+
+            # otherwise, check if the line is a markdown divider, in which case append an html divider to "items"
             elif line.startswith("***") or line.startswith("---"):
-                # line is a divider
                 items += "<tr><td><hr></td><td</td><tr>\n"
+
+        # if there are no items in the file, inform the user instead of showing a blank page
         if items == '':
             items += "<tr><td></td><td><i>No todo items for today.</i></td><tr>\n"
+
     return render_template("KoboTask.html", today=today, items=items)
 
 @app.route('/tick/<today>/<i>/<t>', methods=['POST'])
 def TickItem(today, i, t):
+
     with open(f"{daily_notes}/{today}.md") as note:
         items = note.readlines()
 
+    # boundary check to make sure the requested item to mutate exists
     if int(i) < 0 or int(i) > (len(items)-1):
         return http_exception("416 Request Range Not Satisfiable: No item exists at the specified index (out of bounds).")
 
@@ -99,11 +112,14 @@ def TickItem(today, i, t):
         case "tick" if item.is_ticked():
             item.tick(False)
 
+    # save changes to list item
     items[int(i)] = item.to_md()
 
+    # write changes to the daily note
     with open(f"{daily_notes}/{today}.md", 'w', encoding='utf-8') as note:
         note.writelines(items)
 
+    # redirect back to home page
     return redirect("/#")
 
 
